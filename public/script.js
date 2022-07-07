bc16e82fc192985a3ad0c9b7fdceb93c93c02afb
Rashida Rangwala <r.rang100@gmail.com>
2022-07-07 15:57:36 +0530

    Send Email feature added

--- public/script.js
+++ public/script.js
@@ -105,6 +105,27 @@ $(function () {
             $("#stop_video").html(html)
         }
     })
+
+    $("#invite_button").click(function () {
+        const to = prompt("Enter the email address")
+        let data = {
+            url: window.location.href,
+            to: to
+        }
+        $.ajax({
+            url: "/send-mail",
+            type: "post",
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: "application/json",
+            success: function (result) {
+                alert("Invite Sent !!")
+            },
+            error: function (result) {
+                console.log(result.responseJSON)
+            }
+        })
+    })
 })
 
 peer.on("open", (id) => {
